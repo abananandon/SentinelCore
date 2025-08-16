c4df814e3319d7a935c80e738a1c5b46e2b0c1b7
abananandon <cy2019_ahu@163.com>
2025-08-16 22:07:10 +0800

    Initialize project struct.

---
+++ client/src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+  printf("Hello from Client!\n");
+
+  return EXIT_SUCCESS;
+}
